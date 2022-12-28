2e44970a77d84bbde3aedc30c430d0f911f193c2
Luis Hernandez <lshrnndz6@gmail.com>
2022-12-27 16:55:52 -0800

    commit tree 8eedb77a804747fef941491691761dbc1379abc2

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
 // Code your solution here
+
+function findMatching(drivers, driver){
+    return drivers.filter((person) => person.toLowerCase().includes(driver.toLowerCase()));
+}
+
+function fuzzyMatch(drivers, driver){
+    return drivers.filter((person) => person.startsWith(driver))
+}
+
+function matchName(drivers, prop){
+    return drivers.filter((driver)=> driver.name === prop)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findMatching(drivers, driver){
+//     return drivers.filter((newDriver) => 
+//     newDriver.toLowerCase().includes(driver.toLowerCase()))
+// }
+
+// function fuzzyMatch(arrayOfNames, firstTwoLetters){
+//     const filteredArray = arrayOfNames.filter((name) => name.startsWith(firstTwoLetters));
+//     console.log(filteredArray);
+//     return filteredArray;
+// }
+
+// function matchName (objects, matchingString) {
+//     const objectNames = objects.filter((names)=> names.name === matchingString);
+//     console.log(objectNames);
+//     return objectNames;
+// }
+
